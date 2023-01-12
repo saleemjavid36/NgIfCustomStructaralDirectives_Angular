63f8ce868376548d4d87ae79212023d26ad735d9
saleemjavid36 <saleemjavid360@gmail.com>
2023-01-12 15:34:09 +0530

    CustomNgIfStructuralDirectives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomIfComponent } from './custom-if/custom-if.component';
-import { HeaderComponent } from './header/header.component';
+
 import { CustomShowHideDirectiveDirective } from './custom-show-hide-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomIfComponent,
-    HeaderComponent,
     CustomShowHideDirectiveDirective
   ],
   imports: [
